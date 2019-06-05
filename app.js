ddfa8b8d8f1c47c060857b4ee49d5c43b73f76e2
Stephanie Sapp <stephs1531@gmail.com>
2019-06-04 22:07:11 -0500

    create initial files

--- app.js
+++ app.js
@@ -17,8 +17,38 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
   });
 
-  connection.connect(function(err) {
+  connection.connect(function(err, res) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // start();
+    // console.table(res);
+   
+    start();
   });
+
+  //start function with inquirer prompts
+  function start() {
+    
+    //function to console.table products
+    var query = "SELECT * FROM products";
+    connection.query(query, function(err, res) {
+        console.table(res);
+      });
+      
+    // inquirer prompts
+      inquirer
+        .prompt([
+            {
+          name: "itemID",
+          type: "input",
+          message: "What is the id of the item you wish to buy?",
+            },
+            {
+            name: "quantity",
+            type: "number",
+            message: "How many do you want?"
+            }
+        ])
+        .then(function(answer) {
+         console.log(answer);
+        });
+     };
